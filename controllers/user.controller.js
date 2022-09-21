bc523d0a9a7de63e8780676454193488de380b9c
TyrantJane <nonstop00066@gmail.com>
2022-09-21 20:27:29 +0300

    Add user model

---
+++ controllers/user.controller.js
@@ -0,0 +1,90 @@
+const { Op } = require("sequelize");
+const createError = require('http-errors');
+const { User } = require('../models'); // из db
+
+module.exports.createUser = async (req, res, next) => { //public static --> create --> return Promise (он асинхр)
+    try {
+        const {body} = req;
+        const createdUser = await User.create(body);
+        createdUser.password = undefined;
+        res.status(201).send({data:createdUser});
+    } catch (error) {
+        next(error); // передаёт в app.js
+    }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.findAll({ 
+            attributes: { 
+                exclude: ["password"],
+                //exclude: ["password", "lastName"],
+                //include: [["last_name", "second"]]
+            },
+            // where: {
+            //     email: "tom3@anderson.com"
+            // }
+            // where: {
+            //     id: { [Op.in]: [1, 2, 3] }
+            // }
+        });
+        res.status(200).send({data: users});
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+    try {
+        const { body, params: { userId } } = req;
+        const [row, [updatedUser]] = await User.update(body, {
+            where: {
+                id: userId
+            },
+            returning: true
+        });
+        updatedUser.password = undefined;
+        res.status(200).send({ data: updatedUser });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.updateUserInstance = async (req, res, next) => {
+    try {
+        const { body, instanceUser } = req;
+        const updatedUser = await instanceUser.update(body, { returning: true });
+        updatedUser.password = undefined;
+        res.status(200).send({ data: updatedUser });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.getUser = async (req, res, next) => {
+    try {
+        const { params: { userId } } = req;
+        const user = await User.findByPk(userId, {
+            attributes: { 
+                exclude: ["password"],
+            }
+        });
+        //user.password = undefined;
+        if (!user) {
+            next(createError(404, 'User not found!'));
+        }
+        res.status(200).send({ data: user });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports.deleteUserInstance = async (req, res, next) => {
+    try {
+        const { instanceUser } = req;
+        const result = await instanceUser.destroy();
+        res.status(200).send({ data: instanceUser });
+    } catch (error) {
+        next(error);
+    }
+};
